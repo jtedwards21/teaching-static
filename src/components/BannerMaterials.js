6d4b1c3f7bbb7170c12273f7ea7b54dd22770cef
Joshua Edwards <joshuaedwards@Joshuas-MacBook-Pro.local>
2019-02-12 17:39:43 +0800

    added materials pages

---
+++ src/components/BannerMaterials.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BannerLanding = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>Materials</h1>
+            </header>
+            <div className="content">
+                <p>Educator with more than 10 years of experience,
+                <br/>thousands of classes teaching different levels of learners</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerLanding
